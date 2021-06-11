8b2fe4f795b621e2efbc5e0dedba82b3b87e4d38
CockroachCommunist <83138060+CockroachCommunist@users.noreply.github.com>
2021-06-11 23:55:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.menu-button')
 const menu = document.querySelector('.nav-menu')
 const menuCloseButton = document.querySelector('.menu-close')
+const col = document.querySelector('.trailer')
 
 menuButton.addEventListener("click", () => {
     menu.classList.add("is-active")
@@ -9,4 +10,6 @@ menuButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
     menu.classList.remove("is-active")
     menuCloseButton.classList.remove("is-active")
-})
+});
+
+
